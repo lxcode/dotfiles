b017f1f9ac6d668466bc3647224a5478552b76f1
lxcode <lxcode>
2017-09-14 12:31:39 -0700

    Chase flag change

--- fix_macos.sh
+++ fix_macos.sh
@@ -233,7 +233,7 @@ brew doctor
 brew install task tmux w3m apg bvi cscope daemontools djbdns runit mutt nvi nmap par python3 weechat wireshark youtube-dl bbe zsh w3m vdirsyncer khal ag fzf mobile-shell tree
 # Note that macvim requires full xcode
 brew install macvim --with-python3
-brew install vim --override-system-vi --with-lua --with-python3
+brew install vim --with-override-system-vi --with-lua --with-python3
 brew install ctags --HEAD
 brew install profanity --with-terminal-notifier
 brew linkapps
