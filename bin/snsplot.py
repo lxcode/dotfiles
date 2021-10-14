744a4e4afb5756371cc628acb493b62a2b68cd33
David Thiel <dthiel@stanford.edu>
2021-10-14 10:55:12 -0700

    Tidy, add stacked/dodged histos with multiple hues

--- bin/snsplot.py
+++ bin/snsplot.py
@@ -1,23 +1,27 @@
 #! /usr/bin/env python3
 
+# Takes as input a CSV produced by gF with date and value pinned.
+
 # %% Setup
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sns
 
 title = ""
 
-cardinalred = "#8C1515"
-stanford = ["#175E54", "#279989", "#8F993E", "#6FA287", "#4298B5", "#007C92"]
+cardinalred = ["#8C1515"]
+stanford = ["#8C1515", "#175E54", "#279989", "#8F993E", "#6FA287", "#4298B5", "#007C92"]
 stlight = ["#2D716F", "#59B3A9", "#A6B168", "#8AB8A7", "#67AFD2", "#009AB4"]
 stdark = ["#014240", "#017E7C", "#7A863B", "#417865", "#016895", "#006B81"]
 stbright = ["#E98300", "#E04F39", "#FEDD5C", "#620059", "#651C32", "#5D4B3C"]
 
-lightdark = stlight + stdark
+palette = stanford + stdark
 
-sns.set(font="Noto Sans CJK TC")
+sns.set(font="Roboto")
 sns.set(font_scale=0.8)
-sns.set_palette(sns.color_palette(stanford), n_colors=100)
+sns.set_palette(sns.color_palette(palette), n_colors=100)
+sns.set_style("whitegrid")
 plt.rcParams["axes.labelweight"] = "bold"
 plt.rcParams["axes.titleweight"] = "bold"
 plt.rcParams["axes.titlesize"] = 10
@@ -31,7 +35,7 @@ print(df.head())
 
 
 # %% Calculate top values, remove rest from a tmp dataframe
-top = 6
+top = 10
 toptags = df.tag.value_counts()[:top]
 threshold = toptags.values[top - 1:top][0]
 
@@ -47,7 +51,7 @@ plt.title(title)
 ylabel = "Hashtag"
 
 sns.countplot(
-    y="tag", data=df, order=df.tag.value_counts().iloc[:top].index
+    y="tag", data=df, order=df.tag.value_counts().iloc[:top].index, color=cardinalred
 ).set_ylabel(ylabel)
 
 plt.savefig("/tmp/snsbar.png", dpi=300)
@@ -65,7 +69,7 @@ sns.scatterplot(
     hue="tag",
     hue_order=df.tag.value_counts().iloc[:top].index,
     data=tmpdf,
-    s=5,
+    s=15,
     linewidth=0,
 ).legend().set_title = None
 
@@ -78,7 +82,7 @@ plt.savefig("/tmp/snsplot.pdf")
 tmpdf = df
 tmpdf = tmpdf.set_index("date")
 ldf = tmpdf
-ldf = tmpdf.groupby("tag").resample("H").mean()
+ldf = tmpdf.groupby("tag").resample("D").mean()
 ldf["count"] = ldf["count"].fillna(0)
 
 plt.figure(figsize=(10, 6))
@@ -105,6 +109,17 @@ plt.xticks(rotation=15)
 plt.title(title)
 
 sns.histplot(x="date", data=df, kde=True, bins=666)
+
+plt.savefig("/tmp/snsdist.png", dpi=300)
+plt.savefig("/tmp/snsdist.pdf")
+
+# %% Stacked hist
+plt.figure(figsize=(10, 6))
+plt.xticks(rotation=15)
+plt.title(title)
+
+bins = pd.date_range(start='2013-12-07', end='2014-08-19', freq='1M')
+sns.histplot(x="date", data=df, hue="tag", multiple="dodge", bins=mpl.dates.date2num(bins))
 
 plt.savefig("/tmp/snsdist.png", dpi=300)
 plt.savefig("/tmp/snsdist.pdf")
