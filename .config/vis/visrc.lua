2b158583fb2cb7cebd0af3eb42559c71f1745060
David Thiel <dthiel@stanford.edu>
2022-06-23 15:05:45 +0100

    Use plugin manager for vis

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -12,9 +12,12 @@ vis:command('set autoindent')
 vis:command('set expandtab')
 vis:command('set cursorline')
 vis:command('set number')
-vis:command('map! normal ; :')
-vis:command('map! normal <C-e> :fzf<Enter>')
-vis:command('map! normal ,m :fzfmru<Enter>')
+vis:map(vis.modes.NORMAL, ';', ':')
+vis:map(vis.modes.NORMAL, 'Q', ':q!<Enter>')
+vis:map(vis.modes.NORMAL, '<C-e>', ':fzf<Enter>')
+vis:map(vis.modes.NORMAL, ',m', ':fzfmru<Enter>')
+vis:map(vis.modes.NORMAL, '~', 'g~')
+vis:map(vis.modes.VISUAL, 'gy', '"+y')
 end)
 
 -- builtin plugins
@@ -24,7 +27,19 @@ require('plugins/filetype')
 require('plugins/number-inc-dec')
 
 -- local plugins
-require('plugins/vis-fzf-open/fzf-open')
-require('plugins/vis-ctags/ctags')
-require('plugins/vis-quickfix/init')
-require('plugins/vis-fzf-mru/fzf-mru')
+
+local plug = require('plugins/vis-plug')
+
+-- configure plugins in an array of tables with git urls and options
+local plugins = {
+
+	{ 'kupospelov/vis-ctags' },
+	{ 'erf/vis-sneak' },
+	{ 'peaceant/vis-fzf-mru' },
+	{ 'https://git.sr.ht/~mcepl/vis-fzf-open' },
+	{ 'https://gitlab.com/muhq/vis-spellcheck' },
+	{ 'https://repo.or.cz/vis-quickfix.git' },
+}
+
+-- require and optionally install plugins on init
+plug.init(plugins, true)
