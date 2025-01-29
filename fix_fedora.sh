97159a8d9d2f811de435920a32769976b0afa787
David Thiel <lx-github@grumplicio.us>
2025-01-29 12:59:47 +0000

    Key configs

--- fix_fedora.sh
+++ fix_fedora.sh
@@ -6,7 +6,7 @@ sudo dnf -y remove nano-default-editor
 # pkgs
 sudo dnf install zsh tmux mosh fzf ripgrep mutt mtr curl w3m \
     rust git fd-find khal khard vdirsyncer isync notmuch \
-    glibc-static bat helix vis vim
+    glibc-static bat helix vis vim keyd
 
 cargo install duf dust bottom pv
 pip3 install --user visidata tldextract emoji urlexpander fugashi deepl geopy snownlp lingua-language-detector
@@ -14,10 +14,18 @@ pip3 install --user visidata tldextract emoji urlexpander fugashi deepl geopy sn
 mkdir ~/git
 cd ~/git && git clone https://github.com/lxcode/dotfiles
 cd ~/
-for file in .zshrc .zshenv .zsh .zlogin .vimrc .vim .ctags .editrc .inputrc .nexrc .tmux.confbin .mailcap
+for file in .zshrc .zshenv .zsh .zlogin .vimrc .vim .ctags .editrc .inputrc .nexrc .tmux.conf bin .mailcap
 do
     ln -s ~/git/dotfiles/$file ~/$file
 done
 
 mkdir ~/.w3m
 ln -s ~/git/dotfiles/w3m-config ~/.w3m/config
+
+# caps/esc
+sudo mkdir /etc/keyd
+sudo cp ~/git/dotfiles/keyd-default.conf /etc/keyd/default.conf
+sudo systemctl enable --now keyd
+
+# Get rid of invisible super-o shortcut
+gsettings set org.gnome.settings-daemon.plugins.media-keys rotate-video-lock-static "[]"
